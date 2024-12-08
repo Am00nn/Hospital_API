f16c3090b7c7060446712f6dd4b9a8347196a9e9
Amani <amanialshmali7@gmail.com>
2024-12-08 13:25:39 +0400

    adding IPatientRepository

---
+++ HospitalAPI/Repositories/IPatientRepository.cs
@@ -0,0 +1,10 @@
+﻿using HospitalAPI.Models;
+
+namespace HospitalAPI.Repositories
+{
+    public interface IPatientRepository
+    {
+        void AddPatient(Patient patient);
+        IEnumerable<Patient> GetPatients();
+    }
+}
