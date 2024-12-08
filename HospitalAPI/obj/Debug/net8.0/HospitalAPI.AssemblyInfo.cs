7fe9e3e9a9331ec3d50112123ab1419bc4de03fa
Amani <amanialshmali7@gmail.com>
2024-12-08 13:22:19 +0400

    add Patient modle

--- HospitalAPI/obj/Debug/net8.0/HospitalAPI.AssemblyInfo.cs
+++ HospitalAPI/obj/Debug/net8.0/HospitalAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HospitalAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6dc65fc7211425ba869096802dde8876d1315e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("HospitalAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HospitalAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
