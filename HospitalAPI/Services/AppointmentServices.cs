8f221cd93e7481dc274c9aaf9f8674774d06ee2e
Amani <amanialshmali7@gmail.com>
2024-12-08 13:41:36 +0400

    adding AppointmentServices

---
+++ HospitalAPI/Services/AppointmentServices.cs
@@ -0,0 +1,75 @@
+﻿using HospitalAPI.Models;
+using HospitalAPI.Repositories;
+
+namespace HospitalAPI.Services
+{
+    public class AppointmentServices : IAppointmentServices
+    {
+        private readonly IBookingRepository _bookingRepository;
+
+        private readonly IClinicRepository _clinicRepository;
+
+        public AppointmentServices(IBookingRepository bookingRepository, IClinicRepository clinicRepository)
+        {
+            _bookingRepository = bookingRepository;
+
+
+            _clinicRepository = clinicRepository;
+        }
+
+        public string AddBooking(Booking booking)
+        {
+
+            var clinic = _clinicRepository.GetClinicById(booking.ClinicId);
+
+            if (clinic == null)
+            {
+
+
+                return "Clinic not found";
+
+
+
+            }
+
+
+
+
+            if (clinic.NumberOfSlots > 0)
+            {
+
+                clinic.NumberOfSlots -= 1;
+
+
+                _clinicRepository.UpdateClinic(clinic);
+
+
+                _bookingRepository.BookAppointment(booking);
+
+                return $"Booking Successful. Slots remaining in clinic    : {clinic.NumberOfSlots} ";
+
+            }
+
+
+            return "No available slots in the clinic";
+        }
+
+        public IEnumerable<Booking> ViewAppointmentByClinic(int clinicId)
+        {
+
+
+            return _bookingRepository.ViewAppointmentByClinic(clinicId);
+
+
+        }
+
+        public IEnumerable<Booking> ViewAppointmentByPatient(int patientId)
+        {
+
+
+            return _bookingRepository.ViewAppointmentByPatient(patientId);
+
+
+        }
+    }
+}
