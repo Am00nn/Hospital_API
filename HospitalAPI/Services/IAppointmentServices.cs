f7192ed8fac88489357a7a848cae4e057683b8bd
Amani <amanialshmali7@gmail.com>
2024-12-08 13:41:53 +0400

    adding IAppointmentServices

---
+++ HospitalAPI/Services/IAppointmentServices.cs
@@ -0,0 +1,11 @@
+﻿using HospitalAPI.Models;
+
+namespace HospitalAPI.Services
+{
+    public interface IAppointmentServices
+    {
+        string AddBooking(Booking booking);
+        IEnumerable<Booking> ViewAppointmentByClinic(int clinicId);
+        IEnumerable<Booking> ViewAppointmentByPatient(int patientId);
+    }
+}
