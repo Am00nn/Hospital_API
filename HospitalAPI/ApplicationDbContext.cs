f44b8314f0c303ef9fa279c0721039bd32f509a3
Amani <amanialshmali7@gmail.com>
2024-12-08 13:27:08 +0400

    adding ApplicationDbContext

---
+++ HospitalAPI/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using HospitalAPI.Models;
+using System.Collections.Generic;
+
+namespace HospitalAPI
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Clinic> Clinics { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+    }
+}
