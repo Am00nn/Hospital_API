7e9064a87f80f7422dceebca09155d03d511d980
Amani <amanialshmali7@gmail.com>
2024-12-08 13:36:57 +0400

    adding IPatientService

---
+++ HospitalAPI/Services/IPatientService.cs
@@ -0,0 +1,10 @@
+﻿using HospitalAPI.Models;
+
+namespace HospitalAPI.Services
+{
+    public interface IPatientService
+    {
+        void AddPatient(Patient patient);
+        IEnumerable<Booking> GetPatientAppointments(int patientId);
+    }
+}
