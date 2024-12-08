d951dbbe73ac37e14a4e610a41ff9976c3dce802
Amani <amanialshmali7@gmail.com>
2024-12-08 13:39:01 +0400

    adding ClinicService

---
+++ HospitalAPI/Services/ClinicService.cs
@@ -0,0 +1,29 @@
+﻿using HospitalAPI.Models;
+using HospitalAPI.Repositories;
+
+namespace HospitalAPI.Services
+{
+    public class ClinicService : IClinicService
+    {
+        private readonly IClinicRepository _clinicRepository;
+
+        public ClinicService(IClinicRepository clinicRepository)
+        {
+            _clinicRepository = clinicRepository;
+        }
+
+        public IEnumerable<Clinic> GetAllClinics()
+        {
+
+            return _clinicRepository.GetClinics();
+
+        }
+
+        public void AddClinic(Clinic clinic)
+        {
+
+            _clinicRepository.AddClinic(clinic);
+        }
+
+    }
+}
