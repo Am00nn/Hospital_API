56d17cca18da99410eb67e28f46af19228080d40
Amani <amanialshmali7@gmail.com>
2024-12-08 13:23:12 +0400

    add Clinic modle

--- HospitalAPI/Models/Clinic.cs
+++ HospitalAPI/Models/Clinic.cs
@@ -1,6 +1,19 @@
-﻿namespace HospitalAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HospitalAPI.Models
 {
     public class Clinic
     {
+        [Key]
+        public int CID { get; set; }
+
+        [Required]
+        public string C_Name { get; set; }
+
+        [Required]
+        public string C_Specialization { get; set; }
+
+        [Required]
+        public int NumberOfSlots { get; set; } = 20;
     }
 }
