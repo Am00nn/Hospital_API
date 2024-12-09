b25d1a1d33f539f9ea0da4d1600ff03c13382440
Amani <amanialshmali7@gmail.com>
2024-12-09 09:11:20 +0400

    initial commit

--- HospitalAPI/Program.cs
+++ HospitalAPI/Program.cs
@@ -1,4 +1,8 @@
 
+using HospitalAPI.Repositories;
+using HospitalAPI.Services;
+using Microsoft.EntityFrameworkCore;
+
 namespace HospitalAPI
 {
     public class Program
@@ -7,26 +11,33 @@ namespace HospitalAPI
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddScoped<IPatientRepository, PatientRepository>();
+
+            builder.Services.AddScoped<IClinicRepository, ClinicRepository>();
+
+            builder.Services.AddScoped<IBookingRepository, BookingRepository>();
+
+            builder.Services.AddScoped<IAppointmentServices, AppointmentServices>();
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
+
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
+
                 app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
--- HospitalAPI/obj/Debug/net8.0/HospitalAPI.AssemblyInfo.cs
+++ HospitalAPI/obj/Debug/net8.0/HospitalAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HospitalAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f44b8314f0c303ef9fa279c0721039bd32f509a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3507bb7fb300004687687d1b044cee30ba1a834e")]
 [assembly: System.Reflection.AssemblyProductAttribute("HospitalAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HospitalAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
